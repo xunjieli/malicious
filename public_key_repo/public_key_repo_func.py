1e3cf23db635f76890fd6d2c877dcd32974b26c1
Xunjie Li <xunjieli@gmail.com>
2013-11-26 15:18:09 -0500

    public key repo verification key

--- public_key_repo/public_key_repo_func.py
+++ public_key_repo/public_key_repo_func.py
@@ -1,4 +1,5 @@
 class RepoFuncs:
+
     def set_public_key(self, userid, key):
         with open(userid + '.pkey', 'w+') as f:
                 f.write(key)
@@ -6,3 +7,13 @@ class RepoFuncs:
     def get_public_key(self, userid):
         with open(userid + '.pkey', 'r+') as f:
                 return f.read()
+
+    def set_verification_key(self, userid, key):
+        with open(userid + '.vkey', 'w+') as f:
+                f.write(key)
+        return True
+
+    def get_verification_key(self, userid):
+        with open(userid + '.vkey', 'r+') as f:
+                return f.read()
+
