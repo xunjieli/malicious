72dc679991317a2384c8af6b8a98b40fecfc28f4
Xunjie Li <xunjieli@gmail.com>
2013-11-26 16:46:45 -0500

    server funcs

---
+++ server/server_func.py
@@ -0,0 +1,9 @@
+class ServerFuncs:
+    def begin_authenticate(self, userid):
+        # TODO: generate token and persist in local storage
+        token = 0 
+        return token
+    def end_authenticate(self, userid, token):
+        # TODO:
+        return true
+
