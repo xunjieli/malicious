392c7567e48a12835e09cb47b780dc28a32f9a64
Robin Cheng <hpmv118@gmail.com>
2013-12-02 18:35:32 -0500

    Experimental public key repo new RPC.

--- public_key_repo/public_key_repo_func.py
+++ public_key_repo/public_key_repo_func.py
@@ -1,19 +1,19 @@
 class RepoFuncs:
 
-    def set_public_key(self, userid, key):
+    def rpc_set_public_key(self, userid, key):
         with open(userid + '.pkey', 'w+') as f:
                 f.write(key)
         return True
-    def get_public_key(self, userid):
+    def rpc_get_public_key(self, userid):
         with open(userid + '.pkey', 'r+') as f:
                 return f.read()
 
-    def set_verification_key(self, userid, key):
+    def rpc_set_verification_key(self, userid, key):
         with open(userid + '.vkey', 'w+') as f:
                 f.write(key)
         return True
 
-    def get_verification_key(self, userid):
+    def rpc_get_verification_key(self, userid):
         with open(userid + '.vkey', 'r+') as f:
                 return f.read()
 
