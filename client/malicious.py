beffc8faad867ee64dfe5e98f55364613a018b01
12dmodel <tiam125@gmail.com>
2013-12-03 15:36:27 -0500

    client working with dummy server, still need to fix some of the server API

--- client/malicious.py
+++ client/malicious.py
@@ -10,34 +10,8 @@ from ..common import metadata, crypto
 from ..public_key_repo import public_key_repo_func
 
 client = 0
-#key_repo = xmlrpclib.ServerProxy('http://localhost:8008')
-# fileserver = xmlrpclib.ServerProxy('http://localhost:8000')
-# keep the test local now so I don't have to run separate process to test this
-key_repo = dummykeydist.dummykeydist()
-fileserver = dummyfileserver
 
-def register(name,privatefile):
-	keydist = dummykeydist.dummykeydist()
-	credential = {"max_inode":0L,"MEK":None,"MSK":None}
-	print "creating encryption keys..."
-	MEK = crypto.generate_user_encryption_keypair()
-	MSK = crypto.generate_user_signature_keypair()
-	print "keys generation successful"
-	if MEK is None or MSK is None:
-		print "Error registering the user, nothing has been done"
-		return 1
-	credential['MEK'] = base64.b64encode(crypto.export_key(MEK))
-	credential["MSK"] = base64.b64encode(crypto.export_key(MSK))
-	# upload key to repo
-	key_repo.set_public_key(name,base64.b64encode(crypto.export_key(MEK[0:2])))
-	key_repo.set_verification_key(name,base64.b64encode(crypto.export_key(MSK[0:2])))
-	#key_repo.set_verification_key(name,xmlrpclib.Binary("hahaha2"))
-	#key_repo.set_public_key(name,xmlrpclib.Binary("hahaha"))
-	print "registration succesful"
-	
-	json.dump(credential,open(privatefile,'wb'))
-	
-	return 0
+
 
 def getcmd():
 	global client
@@ -75,6 +49,11 @@ def execcmd(cmd):
 				print "usage: rename [remote source] [new name]"
 				return
 			client.rename(cmd[1],cmd[2])
+		elif cmd[0] == "mv":
+			if len(cmd) < 2:
+				print "usage: mv [remote source] [remote destination]"
+				return
+			client.move(cmd[1],cmd[2])
 		elif cmd[0] == "shr": # share read access
 			if len(cmd) < 3:
 				print "usage: shr [remote file] [user1] [user2] [user3] ..."
@@ -122,24 +101,10 @@ if __name__ =="__main__":
 		print "please log in:"
 		name = raw_input("username:")
 		privatefile = raw_input("private key file:")
-	while not os.path.exists(privatefile):
-		print "cannot find the private key file, do you want to register for a new account (y/n)?"
-		ans = raw_input()
-		if ans == 'y':
-			stat = register(name,privatefile)
-			if stat:
-				print "error while registering for an account, please restart the program"
-				sys.exit(1)
-		else:
-			privatefile = raw_input("re-enter private key file:")
+	
 
-	client = shell.maliciousClient(name,privatefile,fileserver,key_repo)
+	client = shell.maliciousClient(name,privatefile)
 
-	if client.msg != "pass":
-		print "Failed to authenticate: ",client.msg
-		sys.exit(1)
-	else:
-		print "Authentication succeeded, begin typing your first command"
 	if len(sys.argv) > 3:
 		print "Entering script mode"
 		scriptfile = open(sys.argv[3],'r').read()
