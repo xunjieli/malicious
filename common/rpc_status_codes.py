acaed61293d42633a874361481e7e12c53204943
Xunjie Li <xunjieli@gmail.com>
2013-12-03 21:33:08 -0500

    status code

--- common/rpc_status_codes.py
+++ common/rpc_status_codes.py
@@ -3,19 +3,18 @@ WRONG_TOKEN = 2
 ERROR = 3
 
 class RPC_STATUS:
-    def __init___(self, code, error_str):
+    def __init__(self, code, error_str):
         self.code = code
         self.error_str = error_str
 
 class RPC_OK(RPC_STATUS):
     def __init__(self, error_str=None):
-        super(RPC_OK, self).__init__(OK, error_str)
+        RPC_STATUS.__init__(self, OK, error_str)
 
 class RPC_ERROR(RPC_STATUS):
     def __init__(self, error_str=None):
-        super(RPC_ERROR, self).__init__(ERROR, error_str)
+        RPC_STATUS.__init__(self, ERROR, error_str)
 
 class RPC_WRONG_TOKEN(RPC_STATUS):
     def __init__(self, error_str=None):
-        super(RPC_WRONG_TOKEN, self).__init__(WRONG_TOKEN, error_str)
-
+        RPC_STATUS.__init__(self, WRONG_TOKEN, error_str)
